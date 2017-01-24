3bd4acdcccbdfbebd7ab0424bcf4972ffed13f12
jags2-0 <jessica@boomtownroi.com>
2017-01-24 16:14:18 -0500

    Added authors_controller, authors_welcome route, with authors_new html-erb page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
   devise_for :authors
-  get 'blog/posts' => 'blog#post'
 
+  # get '/blogs/new' => 'blogs#new'
+
+    get '/authors/welcome' => 'authors#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
